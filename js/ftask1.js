74d970ec969249deb6dc9c7ed8bb69a1eee2e1d5
jondayful <157879617+jondayful@users.noreply.github.com>
2024-04-11 23:17:08 +0800

    Add JS Final Lab Task 1

---
+++ js/ftask1.js
@@ -0,0 +1,14 @@
+//Part 1
+
+let favoriteColor = Prompt("Please enter your favorite color: ");
+console.log(favoriteColor);
+
+//Part 2
+
+var name = "John";
+var age = 25;
+var city = "New York";
+
+console.log(name);
+console.log(age);
+console.log(city);
